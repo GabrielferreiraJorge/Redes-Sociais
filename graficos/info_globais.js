91bba5b7c43c3146ec179dfde16cca91c8e9845f
Gabriel Ferreira <130074604+GabrielferreiraJorge@users.noreply.github.com>
2024-10-23 10:22:56 -0300

    ....

--- graficos/info_globais.js
+++ graficos/info_globais.js
@@ -1,5 +1,15 @@
 const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
-async function (params) {
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_mundo /1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo /1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minuto = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
     
+    const paragrafo = document.createElement('p')
 }
+
+visualizarInformacoesGlobais()
